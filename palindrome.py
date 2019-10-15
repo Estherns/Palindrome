8a7f888253c3376fa0c2d672c8e375763d617bde
Esther <24371125+Estherns@users.noreply.github.com>
2019-10-15 21:23:00 +0300

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+list1 = ["m", "a", "m"]
+list2 = list1.copy()
+list2.reverse()
+
+#condition to check if the list is palindrome
+
+'''
+if list1 == list2:
+  print("Palindrome")
+else:
+  print("Not palindrome")
